764c99658f3a948e73ea8d62629cb33e8f88ac65
krasow <krasow@u.northwestern.edu>
2025-08-29 12:45:07 -0500

    not sure why things got deleted

---
+++ include/types.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "jlcxx/jlcxx.hpp"
+
+//Wraps the enums which define how legate
+// and cupynumeric types map to legion types
+void wrap_type_enums(jlcxx::Module&);
+
+// Wraps the legate functions which return the
+// specified legate::Type. (e.g. legate::int8())
+void wrap_type_getters(jlcxx::Module&);
+
+// Wraps the privilege modes used in
+// FieldAccessor (AcessorRO, AccessorWO)
+void wrap_privilege_modes(jlcxx::Module&);
